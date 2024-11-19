7a30574690c698c7e724e0b6c00a7d3b2b9bdd1d
mangpoo <dfre3622@naver.com>
2024-11-19 16:50:42 +0900

    대김승운의 수정사항

--- team_H/src/team_H/Main.java
+++ team_H/src/team_H/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello, World!");
+		System.out.println("민석이와 영웅이");
 	}
 
 }
