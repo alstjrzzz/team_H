3b129eb37a23e67293841781d24d9faad8f88c6c
alstjrzzz <fhk5493@gmail.com>
2024-11-27 21:37:26 +0900

    시작화면에 로그인버튼을 넣었습니다람쥐~

--- team_H/src/view/StartGameScreen.java
+++ team_H/src/view/StartGameScreen.java
@@ -2,25 +2,78 @@ package view;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+import javazoom.jl.player.Player;
+import model.GameState;
+
 import java.awt.Graphics;
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.awt.Color;
+import java.awt.GridLayout;
+import javax.swing.JPasswordField;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import java.awt.BorderLayout;
 
-import javazoom.jl.player.Player;
+//import javazoom.jl.player.Player; 처리예정
 
 public class StartGameScreen extends JPanel {
     private ImageIcon backgroundGif;
+    private JTextField textField;
+    private JPasswordField passwordField;
     // private MP3Player bgmPlayer;
     
-    public StartGameScreen() {
+    public StartGameScreen(GameState gameState) {
+       setLayout(null);
+       JPanel panel = new JPanel();
+       panel.setBackground(new Color(255, 255, 255));
+       panel.setBounds(138, 199, 173, 69);
+       add(panel);
+       GridBagLayout loginPanel = new GridBagLayout();
+       loginPanel.columnWidths = new int[] {70, 100};
+       loginPanel.rowHeights = new int[] {2, 2};
+       loginPanel.columnWeights = new double[]{0.0, 0.0};
+       loginPanel.rowWeights = new double[]{0.0, 0.0};
+       panel.setLayout(loginPanel);
+       
+       JLabel lblNewLabel_1 = new JLabel("Username");
+       GridBagConstraints login_ID = new GridBagConstraints();
+       login_ID.fill = GridBagConstraints.HORIZONTAL;
+       login_ID.insets = new Insets(0, 0, 5, 5);
+       login_ID.gridx = 0;
+       login_ID.gridy = 0;
+       panel.add(lblNewLabel_1, login_ID);
+       
+       textField = new JTextField();
+       GridBagConstraints gbc_textField = new GridBagConstraints();
+       gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+       gbc_textField.insets = new Insets(0, 0, 5, 0);
+       gbc_textField.gridx = 1;
+       gbc_textField.gridy = 0;
+       panel.add(textField, gbc_textField);
+       
+       JLabel lblNewLabel = new JLabel("Password");
+       GridBagConstraints login_password = new GridBagConstraints();
+       login_password.fill = GridBagConstraints.HORIZONTAL;
+       login_password.insets = new Insets(0, 0, 5, 5);
+       login_password.gridx = 0;
+       login_password.gridy = 1;
+       panel.add(lblNewLabel, login_password);
+       
+       passwordField = new JPasswordField();
+       GridBagConstraints gbc_passwordField = new GridBagConstraints();
+       gbc_passwordField.insets = new Insets(0, 0, 5, 0);
+       gbc_passwordField.fill = GridBagConstraints.HORIZONTAL;
+       gbc_passwordField.gridx = 1;
+       gbc_passwordField.gridy = 1;
+       panel.add(passwordField, gbc_passwordField);
         // GIF 파일 로드
         backgroundGif = new ImageIcon("res/img/배경화면.gif");
         
-        playBgm(); // 음악 재생
-        
-        // 패널 크기 설정
-        setLayout(null); // 다른 컴포넌트 추가 시 사용할 레이아웃
+        playBgm();
     }
     
     public void playBgm() {
@@ -45,7 +98,7 @@ public class StartGameScreen extends JPanel {
           }
        }.start();
     }
-    
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
