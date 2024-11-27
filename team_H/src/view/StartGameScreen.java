b441b03b943136170211840059fcf8507936a866
alstjrzzz <fhk5493@gmail.com>
2024-11-27 20:32:00 +0900

    model 패키지에 기본 인터페이스와 클래스를 추가 bgm 문제 해결

--- team_H/src/view/StartGameScreen.java
+++ team_H/src/view/StartGameScreen.java
@@ -3,9 +3,11 @@ package view;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import java.awt.Graphics;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 import java.awt.Color;
 
-//import javazoom.jl.player.Player; 처리예정
+import javazoom.jl.player.Player;
 
 public class StartGameScreen extends JPanel {
     private ImageIcon backgroundGif;
@@ -14,15 +16,36 @@ public class StartGameScreen extends JPanel {
     public StartGameScreen() {
         // GIF 파일 로드
         backgroundGif = new ImageIcon("res/img/배경화면.gif");
-        /*
-        bgmPlayer = new MP3Player("res/sound/bgm/로그인창_브금.mp3");
-        bgmPlayer.play(); // 음악 재생
-        처리예정
-        */
+        
+        playBgm(); // 음악 재생
+        
         // 패널 크기 설정
         setLayout(null); // 다른 컴포넌트 추가 시 사용할 레이아웃
     }
-
+    
+    public void playBgm() {
+       Player bgmPlayer = null;
+       String bgmFilePath = "res/sound/bgm/로그인창_브금.mp3";
+       try {
+          FileInputStream fis = new FileInputStream(bgmFilePath);
+          BufferedInputStream bis = new BufferedInputStream(fis);
+          bgmPlayer = new Player(bis);
+       } catch (Exception e) {
+          System.out.println(e.getMessage());
+       }
+       
+       final Player player = bgmPlayer;
+       new Thread() {
+          public void run() {
+             try {
+                player.play();
+             } catch (Exception e) {
+                System.out.println(e.getMessage());
+             }
+          }
+       }.start();
+    }
+    
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
