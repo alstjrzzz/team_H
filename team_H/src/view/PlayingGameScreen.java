04312e6ae5483e5cd666731a7d04a6d7e225c765
noting0113 <tjddhks1725@naver.com>
2024-12-02 22:45:09 +0900

    2024.12.02 캐릭터 선택창 캐릭터 구현 및 게임 플레이 화면 field 움직이는 화면 구현

--- team_H/src/view/PlayingGameScreen.java
+++ team_H/src/view/PlayingGameScreen.java
@@ -5,68 +5,65 @@ package view;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.util.Timer;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 
 import controller.GameController;
 import model.GameState;
 
 public class PlayingGameScreen extends JPanel {
-	
-	private GameState gameState;
-	private GameController gameController;
-	
-	private JPanel healthPanel = new JPanel();
-	private JPanel fieldPanel = new JPanel();
-	private JPanel cardPanel = new JPanel();
+   
+   private GameState gameState;
+   private GameController gameController;
+   
+   private JPanel healthPanel = new JPanel();
+   private JPanel fieldPanel = new FieldPanel();  // FieldPanel을 사용
+   private JPanel cardPanel = new JPanel();
+    private JLabel healthLabel, characterLabel, timerLabel, victoryCountLabel;
+    private JProgressBar healthBar;
+    private Timer gameTimer;
+    private int timeLeft = 60;  // 게임 타이머 (초)
+    private int victoryCount = 0;  // 승리 횟수
 
-	public PlayingGameScreen(GameState gameState, GameController gameController) {
-		
-		this.gameState = gameState;
-		this.gameController = gameController;
-		
-		splitPanel();
-		drawHealthPanel();
-		drawFieldPanel();
-		drawSelectedCardPanel();
-	}
-	
-	
-	public void splitPanel() {
-		
-		setLayout(new BorderLayout());
-		
-		add(healthPanel, BorderLayout.NORTH);
+   public PlayingGameScreen(GameState gameState, GameController gameController) {
+      
+      this.gameState = gameState;
+      this.gameController = gameController;
+      
+      splitPanel();
+      drawHealthPanel();
+      drawSelectedCardPanel();
+   }
+   
+   
+   public void splitPanel() {
+      
+      setLayout(new BorderLayout());
+      
+      add(healthPanel, BorderLayout.NORTH);
         add(fieldPanel, BorderLayout.CENTER);
         add(cardPanel, BorderLayout.SOUTH);
-	}
-	
-	
-	// 상단에 위치한 가로100% 세로10% 비율의 상태칸
-	public void drawHealthPanel() {
-		
-		healthPanel.setBackground(Color.cyan);
-		healthPanel.add(new JLabel("<player1 health bar>  <turn>  <player1 health bar>"));
-		healthPanel.setPreferredSize(new Dimension((int)gameState.getDimension().getWidth()
-													, (int)(gameState.getDimension().getHeight() * 1 / 10)));
-	}
-	
-	
-	public void drawFieldPanel() {
-
-		fieldPanel.setBackground(Color.green);
-		fieldPanel.add(new JLabel("field state"));
-		fieldPanel.setPreferredSize(new Dimension((int)(gameState.getDimension().getWidth())
-														, (int)(gameState.getDimension().getHeight() * 7 / 10)));
-	}
-	
-	
-	public void drawSelectedCardPanel() {
-		
-		cardPanel.setBackground(Color.white);
-		cardPanel.add(new JLabel("<player1 selected card 1, 2, 3>      <player2 selected card 3, 2, 1>"));
-		cardPanel.setPreferredSize(new Dimension((int)(gameState.getDimension().getWidth())
-														, (int)(gameState.getDimension().getHeight() * 2 / 10)));
-	}
+   }
+   
+   
+   // 상단에 위치한 가로100% 세로10% 비율의 상태칸
+   public void drawHealthPanel() {
+      
+      healthPanel.setBackground(Color.cyan);
+      healthPanel.add(new JLabel("<player1 health bar>  <turn>  <player1 health bar>"));
+      healthPanel.setPreferredSize(new Dimension((int)gameState.getDimension().getWidth()
+                                       , (int)(gameState.getDimension().getHeight() * 1 / 10)));
+   }
+   
+   
+   public void drawSelectedCardPanel() {
+      
+      cardPanel.setBackground(Color.white);
+      cardPanel.add(new JLabel("<player1 selected card 1, 2, 3>      <player2 selected card 3, 2, 1>"));
+      cardPanel.setPreferredSize(new Dimension((int)(gameState.getDimension().getWidth())
+                                          , (int)(gameState.getDimension().getHeight() * 2 / 10)));
+   }
 }
