64fe1a7cd7e8790dce0fcef52ba424c5c1fbcdc7
mangpoo <dfre3622@naver.com>
2024-12-22 23:32:18 +0900

    게임종료화면구현 및 킹음짓기 넣음

--- team_H/src/view/EndGameScreen.java
+++ team_H/src/view/EndGameScreen.java
@@ -2,9 +2,17 @@
 
 package view;
 
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.io.FileInputStream;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 import controller.GameController;
+import javazoom.jl.player.Player;
 import model.GameState;
 import network.NetworkManager;
 
@@ -22,21 +30,59 @@ public class EndGameScreen extends JPanel {
 		this.gameController = gameController;
 		this.networkManager = networkManager;
 		
-		// drawResultPanel();
-		// drawRestartButtonPanel();
+		setLayout(new BorderLayout());
+
+	    drawResultPanel();
+	    drawRestartButtonPanel();
 	}
 	
 	
 	
 	public void drawResultPanel() {
-		// 망재앙 스크린 그려줘잉
-		if (gameState.getMyHealth() <= 0 && gameState.getEnemyHealth() <= 0) {
-			System.out.println("무승부");
-		} else if (gameState.getMyHealth() <= 0) {
-			System.out.println("패배");
-		} else if (gameState.getEnemyHealth() <= 0) {
-			System.out.println("승리");
-		}
+		 JLabel resultLabel = new JLabel();
+	        resultLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+	        if (gameState.getMyHealth() <= 0 && gameState.getEnemyHealth() <= 0) {
+	            resultLabel.setIcon(new ImageIcon("res/img/무승부.jpg")); // 무승부 이미지 경로
+	            
+	            gameController.playBGM("res/sound/bgm/무승부_브금.mp3"); // 캐릭터 선택창 브금
+	            new Thread(() -> {
+	                try (FileInputStream fis = new FileInputStream("res/sound/sfx/무승부.mp3")) {
+	                    new Player(fis).play(); // FileInputStream을 바로 사용
+	                } catch (Exception e) {
+	                    System.err.println("Error playing sound: " + e.getMessage());
+	                }
+	            }).start();
+	            resultLabel.setText("무승부");
+	        } else if (gameState.getMyHealth() <= 0) {
+	            resultLabel.setIcon(new ImageIcon("res/img/패배.jpg")); // 패배 이미지 경로
+	            gameController.playBGM("res/sound/bgm/패배_브금.mp3"); // 캐릭터 선택창 브금
+	            new Thread(() -> {
+	                try (FileInputStream fis = new FileInputStream("res/sound/sfx/패배효과음.mp3")) {
+	                    new Player(fis).play(); // FileInputStream을 바로 사용
+	                } catch (Exception e) {
+	                    System.err.println("Error playing sound: " + e.getMessage());
+	                }
+	            }).start();
+	            resultLabel.setText("패배");
+	        } else if (gameState.getEnemyHealth() <= 0) {
+	            resultLabel.setIcon(new ImageIcon("res/img/승리.jpg")); // 승리 이미지 경로
+	            gameController.playBGM("res/sound/bgm/승리_브금.mp3"); // 캐릭터 선택창 브금
+	            new Thread(() -> {
+	                try (FileInputStream fis = new FileInputStream("res/sound/sfx/승리효과음.mp3")) {
+	                    new Player(fis).play(); // FileInputStream을 바로 사용
+	                } catch (Exception e) {
+	                    System.err.println("Error playing sound: " + e.getMessage());
+	                }
+	            }).start();
+	            resultLabel.setText("승리");
+	        }
+
+	        resultLabel.setFont(new Font("Arial", Font.BOLD, 24));
+	        resultLabel.setVerticalTextPosition(SwingConstants.BOTTOM);
+	        resultLabel.setHorizontalTextPosition(SwingConstants.CENTER);
+
+	        add(resultLabel, BorderLayout.CENTER);
 	}
 	
 	
